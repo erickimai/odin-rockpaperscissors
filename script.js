12617e026bd41ac0a1084c79080329ea7d5ca5ba
Erick <erick.imai.rodrigues@gmail.com>
2022-12-28 20:37:09 -0300

    Add score

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll('button');
 const play = document.getElementById('play');
 const p = document.createElement('p');
-const pagecontainer = document.getElementsByClassName('page-container');
 const rules =  document.getElementById('rules');
-
-/*buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    playRound(button.id, getComputerChoice());
-  });
-});*/
+const pagecontainer = document.querySelector('.page-container');
+const rock = document.createElement('button');
+const paper = document.createElement('button');
+const scissors = document.createElement('button');
+const divChoices = document.createElement('div');
+const scoreDiv = document.createElement('div');
+const bottomDiv = document.createElement('div');
+const playerDiv = document.createElement('div');
+const computerDiv = document.createElement('div');
 
 function getComputerChoice(){
   switch(Math.floor(Math.random() * 3)){
@@ -25,37 +26,24 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    alert('Draw');
   } else if (playerSelection === 'Rock') {
       if (computerSelection === 'Scissors') {
-        alert('You won! Rock beats Scissors.');
         playerScore++;
       } else {
-        alert('You lost! Paper beats Rock.');
         computerScore++;
       }
   } else if (playerSelection === 'Paper') {
       if (computerSelection === 'Rock') {
-        alert('You won! Paper beats Rock.');
         playerScore++;
       } else {
-        alert('You lost! Scissors beats Paper.');
         computerScore++;
       }
   } else if (playerSelection === 'Scissors') {
       if (computerSelection === 'Paper') {
-        alert('You won! Scissors beats Paper.');
         playerScore++;
       } else {
-        alert('You lost! Rock beats Scissors');
         computerScore++;
       }
-  }
-  div.textContent = `Player score : ${playerScore} Computer score : ${computerScore}`;
-  if (playerScore === 5) {
-    div.textContent = `You won the game!`;
-  } else if (computerScore === 5) {
-    div.textContent = `You lost the game!`;
   }
 }
 
@@ -64,12 +52,15 @@ play.addEventListener('click', function onClick(e) {
   document.body.style.backgroundColor = 'black';
   play.remove();
   showRules();
+  createButtons();
+  showScore(1);
+  createGame();
 });
 
 function showRules() {
   rules.style.cssText = 'color: white; display: inline-block; font-family: "Roboto Mono", monospace';
   divRules= document.createElement('div');
-  divRules.innerText = 'RULES';
+  divRules.innerText = 'Rules';
   divRules.style.cssText = 'animation: reveal 0.5s; text-decoration: underline';
   rules.appendChild(divRules);
   ul = document.createElement('ul');
@@ -91,4 +82,45 @@ function showRules() {
   ul.appendChild(li2);
   ul.appendChild(li3);
   ul.appendChild(li4);
+}
+
+function createButtons() {
+  rock.innerText = 'ROCK';
+  paper.innerText = 'PAPER';
+  scissors.innerText = 'SCISSORS'
+  rock.setAttribute('id', 'Rock');
+  paper.setAttribute('id', 'Paper');
+  scissors.setAttribute('id', 'Scissors');
+  divChoices.style.cssText = 'display: list-item';
+  pagecontainer.appendChild(divChoices);
+  divChoices.appendChild(rock);
+  divChoices.appendChild(paper);
+  divChoices.appendChild(scissors);
+  const buttons = document.querySelectorAll('button');
+  console.log(buttons);
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let computerChoice = getComputerChoice();
+      playRound(button.id, computerChoice);
+      showScore(0); 
+      playerDiv.innerText = `You chose ${button.id}`;
+      computerDiv.innerText = `Computer chose ${computerChoice}`;
+    });
+  });
+}
+
+function showScore(animate) {
+  scoreDiv.innerHTML = 'Score' + '<br />' + `${playerScore}-${computerScore}`;
+  if (animate === 1) {
+    scoreDiv.style.cssText = "color: white; font-family: 'Roboto Mono', monospace; font-size: 40px; margin: 20px 0 20px 0; animation: reveal 1s; height = 100px";
+  } else {
+    scoreDiv.style.cssText = "color: white; font-family: 'Roboto Mono', monospace; font-size: 40px; margin: 20px 0 20px 0; height = 100px";
+  }
+  pagecontainer.appendChild(scoreDiv);
+  playerDiv.style.cssText = "color: white; font-family: 'Roboto Mono', monospace; font-size: 20px; margin: 20px 0 20px 0; animation: reveal 1s; height = 100px";
+  computerDiv.style.cssText = "color: white; font-family: 'Roboto Mono', monospace; font-size: 20px; margin: 20px 0 20px 0; animation: reveal 1s; height = 100px";
+  bottomDiv.style.cssText = "display: flex; justify-content: space-around";
+  pagecontainer.appendChild(bottomDiv);
+  bottomDiv.appendChild(playerDiv);
+  bottomDiv.appendChild(computerDiv);
 }
